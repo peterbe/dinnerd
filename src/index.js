31f60af4e79e51cbeb29e11068e4cecbb614bc0f
Peter Bengtsson <mail@peterbe.com>
2017-03-29 12:03:06 -0400

    experiments with why-did-you-update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// if (process.env.NODE_ENV !== 'production') {
+//   const {whyDidYouUpdate} = require('why-did-you-update')
+//   whyDidYouUpdate(React)
+// }
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
